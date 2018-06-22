14fd827b43adfbe17bce62ba775c62b49bcca868
Luis Claudio Telles Lima <lclaudio.tl@gmail.com>
2018-06-22 01:19:04 -0300

    first tests

--- Blog/blog/tests.py
+++ Blog/blog/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
-
+from django.core import mail
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse 
 # Create your tests here.
+class UriViewTest(TestCase):
+
+	def test_uri_status_end_templates(self):
+		client = Client()
+		response = client.get(reverse('blog:uri'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'blog/uri_list.html')
+		
